0c33995518fdad7d06335bec52f2fe352a35918f
zhangqinghua <the.patron.saint.of.science@gmail.com>
2025-02-18 01:12:46 +0800

    add

--- src/generate-guid.js
+++ src/generate-guid.js
@@ -13,15 +13,15 @@ const guid = () => {
 
 
 // 1. 读取package.json文件
-const electronJson = JSON.parse(fs.readFileSync("electron-builder.json"));
+// const electronJson = JSON.parse(fs.readFileSync("electron-builder.json"));
 
 // 2. 生成新的GUID(处理window安装bug)
-electronJson.nsis.guid = guid();
+// electronJson.nsis.guid = guid();
 
 // 3. 动态修改应用名称
 const productName = process.env.PRODUCT_NAME || 'unkonw_app'
-electronJson.productName = productName;
+// electronJson.productName = productName;
 
 // 4. 重新写入文件
-fs.writeFileSync("electron-builder.json", JSON.stringify(electronJson, null, 2));
+// fs.writeFileSync("electron-builder.json", JSON.stringify(electronJson, null, 2));
 
