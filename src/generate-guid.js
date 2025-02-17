0b75ce4a3f11e7375c2625902459665830b36b03
zhangqinghua <the.patron.saint.of.science@gmail.com>
2025-02-18 00:30:50 +0800

    add

--- src/generate-guid.js
+++ src/generate-guid.js
@@ -1,4 +1,9 @@
-function guid() { // 这是在网上找的guid生成算法
+const fs = require("fs");
+
+/**
+ * guid生成算法
+ */
+const guid = () => {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0,
       v = c == "x" ? r : (r & 0x3) | 0x8;
@@ -6,18 +11,17 @@ function guid() { // 这是在网上找的guid生成算法
   });
 }
 
-const fs = require("fs");
 
-// 生成新的GUID
-const newGuid = guid();
-
-// 读取package.json文件
+// 1. 读取package.json文件
 const electronJson = JSON.parse(fs.readFileSync("electron-builder.json"));
 
-// 将新的GUID写入package.json文件
-electronJson.nsis.guid = newGuid;
-fs.writeFileSync(
-  "electron-builder.json",
-  JSON.stringify(electronJson, null, 2)
-);
+// 2. 生成新的GUID(处理window安装bug)
+electronJson.nsis.guid = guid();
+
+// 3. 动态修改应用名称
+const productName = process.env.PRODUCT_NAME || 'unkonw_app'
+electronJson.productName = productName;
+
+// 4. 重新写入文件
+fs.writeFileSync("electron-builder.json", JSON.stringify(electronJson, null, 2));
 
