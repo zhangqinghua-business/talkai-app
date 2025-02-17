19eaf483f7356aa93fde4a131879be9ca1f9cac0
zhangqinghua <the.patron.saint.of.science@gmail.com>
2025-02-18 01:21:38 +0800

    add

--- src/generate-guid.js
+++ src/generate-guid.js
@@ -22,7 +22,7 @@ const guid = () => {
 const productName = process.env.PRODUCT_NAME || 'unkonw_app'
 console.log('productName: ', productName);
 
-// electronJson.productName = productName;
+electronJson.productName = productName;
 
 // 4. 重新写入文件
 // fs.writeFileSync("electron-builder.json", JSON.stringify(electronJson, null, 2));
