948459585541b43c9aab3229c9c01924fc7a2fba
zhangqinghua <the.patron.saint.of.science@gmail.com>
2025-02-19 02:12:56 +0800

    add

--- src/generate-guid.js
+++ src/generate-guid.js
@@ -23,7 +23,7 @@ const productName = process.env.PRODUCT_NAME || 'unkonw_app'
 console.log('productName: ', productName);
 
 electronJson.productName = productName;
-// 在 github windows-laestest 环境中不能读取带空格的文件路径，所以直接改最终生成品
+// 在 github windows-laestest 环境中不能读取带空格的文件路径，所以直接修改安装包名字 -> talkai Setup 1.0.0.exe
 electronJson.win.artifactName = productName + '.exe'
 // electronJson.artifactName = productName;
 
